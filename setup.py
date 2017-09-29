8218e5c7d04bd51df3696160f2f0fa827d075c16
Viral Joshi <viral@linxsystems.com>
2017-09-29 16:17:53 -0500

    Accounts app created. "base.html" file created to extend all views to the file. "setup.py" file created for setup.
    -- 09/29/2017 -- Viral Joshi

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+INSTALL_REQUIREMENTS = [
+    'Django>=1.11',
+    'mysqlclient>=1.3.10',
+    'setuptools>=36.2.2',
+    'pytz>=2017.2',
+]
+
+CLASSIFIERS = [
+    'Development Status :: 1 - Development',
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+    'Programming Language :: Python :: 3.6',
+    'Framework :: Django',
+    'Framework :: Django :: 1.11',
+]
+
+setup(
+    author='Viral Joshi and contributors',
+    author_email='',
+    name='Rekha.Io',
+    version='1.0.0',
+    description='Rekha.Io : Open Source project to share programming skills',
+    url='https://github.com/viralj/Rekha.Io',
+    license='BSD License',
+    platforms=['OS Independent'],
+    classifiers=CLASSIFIERS,
+    install_requires=INSTALL_REQUIREMENTS,
+    packages=find_packages(exclude=['project', 'project.*']),
+    include_package_data=True,
+    zip_safe=False,
+)
