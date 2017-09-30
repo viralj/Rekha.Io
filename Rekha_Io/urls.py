ddeb0f757b953e9773788f393c0c8e9a6254502b
Viral Joshi <viral@linxsystems.com>
2017-09-30 12:14:28 -0500

    robots.txt file created and added in url.
    -- 09/30/2017 -- Viral Joshi

--- Rekha_Io/urls.py
+++ Rekha_Io/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 # from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
+    
+    url(r'^robots.txt$', TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+        name="robots_file"),
 ]
