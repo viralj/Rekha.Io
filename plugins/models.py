0b1f3502e6930861c26bb55dd0b004a122d23f41
Viral Joshi <viral@linxsystems.com>
2017-10-07 14:09:34 -0500

    Plugins model created.
    -- 10/07/2017 -- Viral Joshi

--- plugins/models.py
+++ plugins/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
 
 # Create your models here.
+
+
+class Plugins(models.Model):
+    """
+    This Plugins model will hold plugin details whether it is active or not.
+    """
+    plugin_hash = models.CharField(_('plugin unique hash'), max_length=250, null=False)
+    is_active = models.BooleanField(_('is active?'), default=False, help_text=_('is plugin active?'))
