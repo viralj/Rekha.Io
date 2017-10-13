0278b3edd16bed1a24b62bd022a309c724356eda
Viral Joshi <viraljoshi23@live.com>
2017-10-13 14:41:03 -0500

    Default plugins set to true in model. Account action url updated to serve page when appending slash is found in url.
    -- 10/13/2017 -- Viral Joshi

--- plugins/models.py
+++ plugins/models.py
@@ -10,4 +10,4 @@ class Plugins(models.Model):
     This Plugins model will hold plugin details whether it is active or not.
     """
     plugin_hash = models.CharField(_('plugin unique hash'), max_length=250, null=False)
-    is_active = models.BooleanField(_('is active?'), default=False, help_text=_('is plugin active?'))
+    is_active = models.BooleanField(_('is active?'), default=True, help_text=_('is plugin active?'))
