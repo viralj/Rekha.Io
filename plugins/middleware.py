cf780b56c460ede9cf02ecf70f749950df8a7305
Viral Joshi <viraljoshi23@live.com>
2017-10-13 14:05:35 -0500

    Middleware for logged in user access updated.
    -- 10/13/2017 -- Viral Joshi

--- plugins/middleware.py
+++ plugins/middleware.py
@@ -1,13 +1,35 @@
 """
 This middleware file will contain most plugin related middleware and related middleware.
 """
+from re import compile
+
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.utils.deprecation import MiddlewareMixin
 
 __author__ = "Viral Joshi"
 
 
-class LoggedInRedirectMiddleware(object):
-    def __init__(self, get_response):
-        self.get_response = get_response
+class LoggedInRedirectMiddleware(MiddlewareMixin):
+    """
+    To redirect logged in users from specific urls.
 
-    def __call__(self, request):
-        return self.get_response(request)
+    For eg: Redirect from Login page, Sign up page
+
+    """
+
+    def process_request(self, request):
+        # Current path
+        path = request.path_info.lstrip('/')
+
+        # List of urls to redirect logged in users from
+        redirect_urls = [
+            reverse('accounts:action').lstrip('/'),
+            reverse('accounts:action_login').lstrip('/'),
+            reverse('accounts:action_signup').lstrip('/')
+        ]
+
+        if path in redirect_urls and request.user.is_authenticated:
+            return HttpResponseRedirect("/")
+
+        return None
