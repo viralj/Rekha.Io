1e099beb407fe0aa6e55b463dc0c6a1a849bcae6
Viral Joshi <viraljoshi23@live.com>
2017-10-14 15:05:15 -0500

    React js installed.
    -- 10/14/2017 -- Viral Joshi

---
+++ questions/urls.py
@@ -0,0 +1,10 @@
+"""
+Url file for all accounts related urls
+"""
+
+from django.conf.urls import url
+
+urlpatterns = [
+    # to display signup and login forms
+    # url(r'^action/?$', RIAccountsAction.as_view(), name="action"),
+]
