b0a2ce353684b284baeaaf4de0522ab960cd9241
Viral Joshi <viraljoshi23@live.com>
2017-10-18 16:58:47 -0500

    Homepage updated. Babel cdn updated. Test js file created.
    -- 10/18/2017 -- Viral Joshi

---
+++ static/js/home.app.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Viral Joshi on 10/18/17.
+ *
+ * This is home js file which will contain all home page related frontend code for ReactJS.
+ */
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello World!!!
+            </div>
+        );
+    }
+}
+
+
+ReactDOM.render(<App />, document.getElementById('home-app'));
