5f87de39d75d41b14c5cd7bab002f8c2ec56e642
Viral Joshi <viraljoshi23@live.com>
2017-10-31 16:13:43 -0500

    Time difference function added, to get time of two days ago.
    -- 10/31/2017 -- Viral Joshi

--- Rekha_Io/general.py
+++ Rekha_Io/general.py
@@ -7,3 +7,11 @@ def two_days_hence():
     :return:
     """
     return timezone.now() + timezone.timedelta(days=2)
+
+
+def two_days_ago():
+    """
+    To return time after 48 hours.
+    :return:
+    """
+    return timezone.now() - timezone.timedelta(days=2)
