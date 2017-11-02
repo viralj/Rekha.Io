a1a741f8610fa597cfc0cb25e280c49def43daf3
Viral Joshi <viraljoshi23@live.com>
2017-11-01 20:39:57 -0500

    General function created to get login paramed url.
    -- 11/01/2017 -- Viral Joshi

--- Rekha_Io/general.py
+++ Rekha_Io/general.py
@@ -1,4 +1,8 @@
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.utils import timezone
+
+from plugins.request_params import get_request_param, REQUEST_LOGIN
 
 
 def two_days_hence():
@@ -15,3 +19,7 @@ def two_days_ago():
     :return:
     """
     return timezone.now() - timezone.timedelta(days=2)
+
+
+def get_ri_login_with_param():
+    return HttpResponseRedirect(reverse('accounts:action') + get_request_param(REQUEST_LOGIN))
