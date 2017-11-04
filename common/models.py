df027689787cecfbc5cf7089a7e4281dd6eb7c62
Viral Joshi <viraljoshi23@live.com>
2017-11-04 15:26:21 -0500

    Common Tag model created.
    -- 11/04/2017 -- Viral Joshi

--- common/models.py
+++ common/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    """
+    Tag common model to attach tags in questions, tutorials etc
+    """
+    name = models.CharField(max_length=254, unique=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
