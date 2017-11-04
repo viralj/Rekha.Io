c27dca351e0ed7230b8faa0ee63a015b9c41eb07
Viral Joshi <viraljoshi23@live.com>
2017-11-04 15:27:46 -0500

    Ask questions url updated.
    -- 11/04/2017 -- Viral Joshi

--- questions/urls.py
+++ questions/urls.py
@@ -4,7 +4,11 @@ Url file for all accounts related urls
 
 from django.conf.urls import url
 
+from questions.views import RIAskQuestion
+
 urlpatterns = [
     # to display signup and login forms
     # url(r'^action/?$', RIAccountsAction.as_view(), name="action"),
+
+    url(r'^ask?$', RIAskQuestion.as_view(), name="ask"),
 ]
