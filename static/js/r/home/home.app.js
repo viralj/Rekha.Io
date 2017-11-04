9c33503235c8011d7f950ed7d176a1ef37634270
Viral Joshi <viraljoshi23@live.com>
2017-11-04 15:27:00 -0500

    Comments removed.
    -- 11/04/2017 -- Viral Joshi

--- static/js/r/home/home.app.js
+++ static/js/r/home/home.app.js
@@ -1,9 +1,3 @@
-/**
- * Created by Viral Joshi on 10/18/17.
- *
- * This is home js file which will contain all home page related frontend code for ReactJS.
- */
-
 class App extends React.Component {
     render() {
         return (
