484fd8278755e617f788b48a748fc780a552bf45
Viral Joshi <viraljoshi23@live.com>
2017-11-04 15:49:44 -0500

    Form helper get json form method updated to check attributes in object to handle errors.
    -- 11/04/2017 -- Viral Joshi

--- plugins/form_helper.py
+++ plugins/form_helper.py
@@ -2,11 +2,15 @@ import json
 
 from django import forms
 
+from Rekha_Io.settings import DEBUG
+
 
 class RIHelperModelForm(forms.ModelForm):
     """
     Helper model form so other model forms can extend and inherit following methods.
     """
+
+    x = {}
 
     def __init__(self, *args, **kwargs):
         super(RIHelperModelForm, self).__init__(*args, **kwargs)
@@ -16,26 +20,41 @@ class RIHelperModelForm(forms.ModelForm):
         for name, field in self.fields.items():
             result.append({'id': "id_{}".format(name), 'data': self.field_to_dict(field)})
 
-        print(result)
-        print(json.dumps(result))
-
-        return ""
+        return json.dumps(result)
 
     def field_to_dict(self, field):
-        x = {
-            "type": str(field.__class__.__name__),
-            "hidden": field.widget.is_hidden,
-            "required": field.widget.is_required,
-            "label": field.label,
-            "help_text": field.help_text,
-            "min_length": field.min_length,
-            "max_length": field.max_length,
-            "initial_value": field.initial,
-            "widget": str(field.widget.__class__.__name__),
-            "attrs": field.widget.attrs,
-        }
+
+        if not DEBUG:
+            self.x = {
+                "type": str(field.__class__.__name__),
+                "widget": str(field.widget.__class__.__name__),
+            }
+
+        if hasattr(field, 'help_text'):
+            self.x.update({"help_text": field.help_text})
+
+        if hasattr(field, 'label'):
+            self.x.update({"label": field.label})
+
+        if hasattr(field, 'initial'):
+            self.x.update({"initial_value": field.initial})
+
+        if hasattr(field, 'min_length'):
+            self.x.update({"min_length": field.min_length})
+
+        if hasattr(field, 'max_length'):
+            self.x.update({"max_length": field.max_length})
+
+        if hasattr(field, 'widget.is_hidden'):
+            self.x.update({"hidden": field.widget.is_hidden})
+
+        if hasattr(field.widget, 'is_required'):
+            self.x.update({"required": field.widget.is_required})
+
+        if hasattr(field.widget, 'attrs'):
+            self.x.update({"attrs": field.widget.attrs})
 
         if 'data-field-type' in field.widget.attrs:
-            x.update({"field_type": field.widget.attrs['data-field-type']})
+            self.x.update({"field_type": field.widget.attrs['data-field-type']})
 
-        return x
+        return self.x
