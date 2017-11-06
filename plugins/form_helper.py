0e86bd1f8c49f66eaf090b43c8fe942964ebe26a
Viral Joshi <viraljoshi23@live.com>
2017-11-06 16:09:19 -0600

    Removed class variable to avoid problem of missing data.
    -- 11/06/2017 -- Viral Joshi

--- plugins/form_helper.py
+++ plugins/form_helper.py
@@ -10,8 +10,6 @@ class RIHelperModelForm(forms.ModelForm):
     Helper model form so other model forms can extend and inherit following methods.
     """
 
-    x = {}
-
     def __init__(self, *args, **kwargs):
         super(RIHelperModelForm, self).__init__(*args, **kwargs)
 
@@ -19,42 +17,52 @@ class RIHelperModelForm(forms.ModelForm):
         result = []
         for name, field in self.fields.items():
             result.append({'id': "id_{}".format(name), 'data': self.field_to_dict(field)})
-
         return json.dumps(result)
 
     def field_to_dict(self, field):
-
+        input_field = {}
+        x = {}
         if not DEBUG:
-            self.x = {
+            x = {
                 "type": str(field.__class__.__name__),
                 "widget": str(field.widget.__class__.__name__),
             }
 
+        # help text for input
         if hasattr(field, 'help_text'):
-            self.x.update({"help_text": field.help_text})
+            x.update({"help_text": field.help_text})
 
+        # label for input
         if hasattr(field, 'label'):
-            self.x.update({"label": field.label})
+            x.update({"label": field.label})
 
+        # place holder object for for input
         if hasattr(field, 'initial'):
-            self.x.update({"initial_value": field.initial})
+            input_field.update({"placeholder": field.initial})
 
+        # min length object for for input
         if hasattr(field, 'min_length'):
-            self.x.update({"min_length": field.min_length})
+            input_field.update({"min_length": field.min_length})
 
+        # max object for for input
         if hasattr(field, 'max_length'):
-            self.x.update({"max_length": field.max_length})
+            input_field.update({"max_length": field.max_length})
 
+        # hidden object for for input
         if hasattr(field, 'widget.is_hidden'):
-            self.x.update({"hidden": field.widget.is_hidden})
+            input_field.update({"hidden": field.widget.is_hidden})
 
+        # is required object for for input
         if hasattr(field.widget, 'is_required'):
-            self.x.update({"required": field.widget.is_required})
+            input_field.update({"required": field.widget.is_required})
 
+        # all attributes for for input
         if hasattr(field.widget, 'attrs'):
-            self.x.update({"attrs": field.widget.attrs})
+            x.update({"attrs": field.widget.attrs})
 
-        if 'data-field-type' in field.widget.attrs:
-            self.x.update({"field_type": field.widget.attrs['data-field-type']})
+        # type object for for input
+        if hasattr(field.widget.attrs, 'data-field-type'):
+            input_field.update({"type": field.widget.attrs['data-field-type']})
 
-        return self.x
+        x.update({"input": input_field})
+        return x
