14fd6784863fad1a426735046e1af784eb9d839e
Viral Joshi <viraljoshi23@live.com>
2017-11-06 17:17:35 -0600

    Deleted compressed js files.
    -- 11/06/2017 -- Viral Joshi

--- compressed/assets/js/40678d4bf241.js
+++
@@ -1,2 +0,0 @@
-;$(".button-collapse").sideNav();$(".dropdown-button").dropdown();;class App extends React.Component{render(){return(React.createElement("div",{className:"container"},React.createElement("div",{className:"row"},React.createElement("div",{className:"col s12 m12 l12"},React.createElement("p",null,"Hello World!!!"),React.createElement("p",null,"This is test file!")))));}}
-ReactDOM.render(React.createElement(App,null),document.getElementById('home-app'));
