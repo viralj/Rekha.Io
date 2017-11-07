d6ddff83e9f6dc0423a1e65935700a9f1d8edb69
Viral Joshi <viraljoshi23@live.com>
2017-11-07 09:37:51 -0600

    Test code for ask question form in JSX file and compiled version.
    -- 11/07/2017 -- Viral Joshi

--- static/js/r/questions/questions.ask.js
+++ static/js/r/questions/questions.ask.js
@@ -1,17 +1,41 @@
 class App extends React.Component {
 
-    createForm(){
+    createForm() {
         let form_fields = [];
+        let csrf_token = document.getElementsByName('csrf_token');
+        csrf_token = csrf_token.getAttribute('content');
+        alert(csrf_token);
 
-        for(let i=0;i<RIAskQ.form.length;i++){
-            console.log(RIAskQ.form[i]);
+        for (let i = 0; i < RIAskQ.form.length; i++) {
+            let data_parent = "";
+            let input_field = "";
+            data_parent += Object.entries(RIAskQ.form[i].data.attrs.data_parent).map(([key, value]) => {
+                return ` ${key}="${value.toString()}"`;
+            });
+
+            input_field += Object.entries(RIAskQ.form[i].data.input_field).map(([key, value]) => {
+                if(value != null)
+                    return ` ${key}="${value.toString()}"`;
+            });
+
+            form_fields.push(
+                `<div className="row">
+                    <div ${data_parent.toString()}>
+                        <input ${input_field.toString()}/>
+                        <label for="${RIAskQ.form[i].data.label}">${RIAskQ.form[i].data.label}</label>
+                    </div>
+                </div>`
+            );
+
         }
+        let form = `<form method="post" action="${RIAskQ.post}">${form_fields.toString()}</form>`;
+        return(React.createElement("div", null, "this"))
 
     }
 
     render() {
 
-        this.createForm();
+        console.log(this.createForm());
 
         return (
             React.createElement("div", {className: "container"}, 
